6d564be1d9ab2ad3b31ec9cced0b2cd0a5e0a1d0
wuyangjiacuo <wuyangjiacuo@gmail.co>
2020-06-11 08:53:33 +0800

    初始化环境

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# react-demos
-react使用方法和特性的demo集合
+# react-notebook
+用react构建的笔记本应用
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import CreateElementDemo from './demos/CreateElementDemo'
+import DataBind from './demos/DataBind'
+
+class App extends Component {
+    render() {
+        return ( 
+          <div >
+            <DataBind/>
+          </div>
+        )
+    }
+}
+
+export default App
